4218205863db6a31fc95c13aa37e323a5c220906
akky741 <148666609+akky741@users.noreply.github.com>
2024-03-11 19:18:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+def FeedbackHub(Name, Phone, Email,Review):
+    df = pd.read_csv("Feedback_file.csv", encoding="utf-8")
+    new_data = {"Name": [Name], "Phone": [Phone], "Email": [Email],"Review":[Review]}
+    df = df._append(pd.DataFrame(new_data), ignore_index=True)
+    df.to_csv("Feedback_file.csv", encoding="utf-8", index=False)
+
+
+def main():
+    st.title("User Entry Form")
+
+    Name = st.text_input("Name:")
+    Phone = st.text_input("Phone:")
+    Email = st.text_input("Email:")
+    Review = st.text_input("Review:")
+
+    if st.button("Submit"):
+        if Name and Phone and Email and Review:
+            FeedbackHub(Name, Phone, Email,Review)
+            st.success("Entry submitted successfully!")
+        else:
+            st.warning("Please fill in all the fields correctly.")
+
+if __name__ == "__main__":
+    main()
+
